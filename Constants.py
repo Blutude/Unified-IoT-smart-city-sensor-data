83665fda1bd602f20aeb04ebdd88bfc2eab83317
rony.azrak <rony.azrak@GOLSHIFETH.BCRL.mcgill.ca>
2017-07-18 15:43:53 -0400

    added Constants.py with no key or url

---
+++ Constants.py
@@ -0,0 +1,15 @@
+class Constants:
+
+    def __setattr__(self, name, value):
+        if hasattr(self, name):
+            raise Exception("Attempting to alter read-only value")
+
+        self.__dict__[name] = value
+
+    ## All final constant values
+    FILE_SIZE_LIMIT = 25000
+    URL = ''
+    KEY = ''  # primary key
+    RADAR_DATABASE_NAME = 'BCRL-TLN-database'
+    RADAR_COLL_NAME = 'BCRL-TLN-collection'
+    ADDRESSES = ['192.168.10.50', '192.168.10.51', '192.168.10.52', '192.168.10.53']
