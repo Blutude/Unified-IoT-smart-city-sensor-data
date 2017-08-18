a6379e271d838d79c28ff8e7707dbdec281e13ce
rony.azrak <rony.azrak@gmail.com>
2017-08-18 17:08:35 -0400

    Fixed https connection bug. Now program automatically re-attempts connection.

--- Azure.py
+++ Azure.py
@@ -9,17 +9,25 @@ def getCosmosDBClient():
 def getCosmosDBColl_link():
     client = getCosmosDBClient()
 
-    db_id = Constants.DATABASE_NAME
-    db_query = "select * from r where r.id = '{0}'".format(db_id)
-    db = list(client.QueryDatabases(db_query))[0]
-    db_link = db['_self']
+    success = False
+    while not success:
+        try:
+            db_id = Constants.DATABASE_NAME
+            db_query = "select * from r where r.id = '{0}'".format(db_id)
+            db = list(client.QueryDatabases(db_query))[0]
+            db_link = db['_self']
 
-    coll_id = Constants.COLL_NAME
-    coll_query = "select * from r where r.id = '{0}'".format(coll_id)
-    coll = list(client.QueryCollections(db_link, coll_query))
-    if coll:
-        coll = coll[0]
-    else:
-        raise ValueError("Collection not found in database.")
-    coll_link = coll['_self']
+            coll_id = Constants.COLL_NAME
+            coll_query = "select * from r where r.id = '{0}'".format(coll_id)
+            coll = list(client.QueryCollections(db_link, coll_query))
+            if coll:
+                coll = coll[0]
+            else:
+                raise ValueError("Collection not found in database.")
+            coll_link = coll['_self']
+            success = True
+        except Exception as e:
+            print(str(e))
+    print("Successfully connected to database")
+
     return coll_link
